762c4510930328af204b1d7c69011d0b61bd6113
thormighti <peterabeshi4@gmail.com>
2022-09-09 17:42:23 +0100

    finished up on the without div symbol work

---
+++ src/lib.rs
@@ -0,0 +1,92 @@
+//! # Without Divisor Symbol
+//!
+//! This crate is just a fun play not to be taken serious. if you realy want to do division,
+//! just use the divisor(/) symbol in your program. This is just for learning purposes.
+//!
+
+
+/// contains the geneic function ```divide()``` which takes generic type of 2 numbers and returns their resut
+/// 
+/// # Example
+/// 
+/// ```
+/// ```
+/// let a :u32 = 7;
+/// let b : i32 = 2;
+/// 
+/// assert_eq!(3, without_div_sym::divide(a,b));
+/// ```
+///    
+/// 
+/// # Problems
+/// 
+/// This crate doesnt accept floating point number yet. i will work on that on next versions.
+/// the example below will cause an error 
+/// 
+/// ```
+/// let a = 15.0;
+/// let b = 5.0;
+/// assert_eq!(3.0, without_div_sym::divide(a,b)); // This wont complie, it will bring out an error
+/// 
+extern crate num;
+
+pub use self::wo_divisor::divide;
+pub mod wo_divisor {
+
+    use num::{one, zero, One, Zero};
+
+    pub fn divide<T>(mut dividend: T, mut divisor: T) -> T
+    where
+        T: std::cmp::PartialOrd
+            + std::ops::Not<Output = T>
+            + std::ops::Add<T, Output = T>
+            + std::ops::ShlAssign<T>
+            + std::ops::SubAssign<T>
+            + One
+            + Copy
+            + Zero,
+    {
+        //-> Option<i32>{
+        //when the divisor is zero
+
+        let checkzero: T = zero();
+        let theone: T = one();
+
+        if divisor == checkzero {
+            // None
+            panic!("");
+        }
+        //to handle negative numbers
+        let mut is_negative = false;
+        if dividend < checkzero {
+            //change the sign
+            dividend = !dividend + theone;
+            is_negative = !is_negative;
+        }
+
+        if divisor < checkzero {
+            divisor = !divisor + theone;
+            is_negative = !is_negative;
+        }
+
+        let mut result: T = one();
+        let mut denominator = divisor;
+        // Double divisor value with bitwise shift until bigger than dividend
+
+        while dividend > denominator {
+            denominator <<= theone;
+            result <<= theone;
+        }
+        // Subtract divisor value until denominator is smaller than dividend
+        while denominator > dividend {
+            denominator -= divisor;
+            result -= one();
+        }
+
+        // If one of dividend or divisor was negative, change sign of result
+        if is_negative {
+            result = !result + theone;
+        }
+        result
+    }
+}
---
+++ tests/integration_test.rs
@@ -0,0 +1,7 @@
+use without_div_sym::divide;
+
+#[test]
+
+fn test_divide(){
+    assert_eq!(10, divide(50, 5));
+}
